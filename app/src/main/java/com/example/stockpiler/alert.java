4b5e21b2db1749dce37fede7e4e27adb52d6a3a4
Jeeva Buyju <32033782+jeevabuyju@users.noreply.github.com>
2021-01-10 23:44:53 +0530

    Tweeks

--- app/src/main/java/com/example/stockpiler/alert.java
+++ app/src/main/java/com/example/stockpiler/alert.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
